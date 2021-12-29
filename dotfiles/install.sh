f581aff80b9a3d13959a98241942d2323df83c08
marzeq <marzeqmarzeq@gmail.com>
2021-12-29 16:46:44 +0100

    added pacstall, doas, vim-plug and nvm moved discord and neovim to pactsall

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###############################################################################################################
-# This script is a little installer for my dotfiles, programs I use frequently, and other random stuff  need #
+# This script is a little installer for my dotfiles, programs I use frequently, and other random stuff I need #
 # DISCLAIMER: this script is NOT an installer script, it's meant to be run after a fresh install!             #
 ###############################################################################################################
 
@@ -29,7 +29,7 @@ cp "$currentdir"/init.vim "$HOME"/.config/nvim/init.vim
 # yay #
 #######
 
-# we'll need this later to install appimagelauncher
+# we'll need this later to install appimagelauncher on arch based distros
 
 if command -v pacman &>/dev/null; then
     if command -v yay &>/dev/null; then
@@ -43,10 +43,27 @@ if command -v pacman &>/dev/null; then
     fi
 fi
 
+############
+# pacstall #
+############
+
+# we'll need this later to install neovim on arch based distros
+
+if command -v apt &>/dev/null; then
+	if command -v pacstall &>/dev/null; then
+		echo "pacstall is already installed"
+	else
+		echo "Installing pacstall"
+		sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+	fi
+fi
+
 
 ####################
 # AppImageLauncher #
 ####################
+
+# we'll need this later to install lunar client
 
 if command -v appimagelauncherd &> /dev/null; then
     echo "AppimageLauncher is already installed"
@@ -110,9 +127,7 @@ if command -v discord &> /dev/null; then
 else
     echo "Installing Discord"
     if command -v apt &> /dev/null; then
-        wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-        sudo dpkg -i discord.deb
-        rm discord.deb
+        pacstall -I discord
     elif command -v pacman &> /dev/null; then
         sudo pacman -S discord
     else
@@ -128,19 +143,16 @@ fi
 echo ""
 
 
-##########
-# Neovim #
-##########
+#####################
+# Neovim & vim-plug #
+#####################
 
 if command -v nvim &> /dev/null; then
     echo "Neovim is already installed"
 else
     echo "Installing Neovim"
     if command -v apt &> /dev/null; then
-        sudo apt install software-properties-common
-        sudo add-apt-repository ppa:neovim-ppa/stable
-        sudo apt update
-        sudo apt install neovim
+    	pacstall -I neovim
     elif command -v pacman &> /dev/null; then
         sudo pacman -S neovim
     elif command -v dnf &> /dev/null; then
@@ -149,11 +161,57 @@ else
         echo "No supported package manager found for installing Neovim, please install Neovim manually: https://github.com/neovim/neovim"
         exit 1
     fi
+    echo ""
+    echo "Installing vim-plug"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
 echo ""
 
+######################
+# NodeJS through nvm #
+######################
 
+echo "Installing NodeJS and nvm"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+
+echo ""
+
+########
+# doas #
+########
+
+echo "Installing doas"
+
+if command -v pacman &> /dev/null; then
+	yay -S doas
+else
+	if command -v apt &> /dev/null; then
+		sudo apt install build-essential make bison flex libpam0g-dev
+	elif command -v dnf &> /dev/null; then
+		sudo dnf install gcc gcc-c++ make flex bison pam-devel byacc git
+	elif command -v yum &> /dev/null; then
+		sudo yum install gcc gcc-c++ make flex bison pam-devel byacc git
+	elif command -v zypper &> /dev/null; then
+		sudo zypper install gcc gcc-c++ make flex bison pam-devel byacc git
+	else
+		echo "No supported package manager found for installing doas, please install Neovim manually: https://github.com/slicer69/doas#installing-build-tools"
+        exit 1
+    fi
+	git clone https://github.com/slicer69/doas
+	cd doas
+	make && sudo make install
+	cd .. && rm -rf doas
+	sudo echo "permit ${USER} as root" > /usr/local/etc/doas.conf
+fi
+
+echo ""
+	
 ################
 # Lunar Client #
 ################
@@ -161,7 +219,7 @@ echo ""
 read -p "Do you want to install Lunar Client? [y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    wget -O LunarClient.AppImage "https://launcherupdates.lunarclientcdn.com/Lunar%20Client-2.8.5.AppImage"
+    wget -O LunarClient.AppImage "https://launcherupdates.lunarclientcdn.com/Lunar%20Client-2.9.3.AppImage"
     xdg-open LunarClient.AppImage
     echo "!!! SELECT INTEGRATE AND RUN !!!"
     echo "!!! SELECT INTEGRATE AND RUN !!!"
